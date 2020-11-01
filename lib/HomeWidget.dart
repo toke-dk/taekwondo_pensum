6014dbf79e06cec05f882942e6d43c3a331af77a
Toke Friis <tokefriis@gmail.com>
2020-11-01 20:58:21 +0100

    file for belts general. And made 2. kup

--- lib/HomeWidget.dart
+++ lib/HomeWidget.dart
@@ -1,90 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:taekwondopensum/MakeBelt.dart';
 import 'package:taekwondopensum/pages/IngameWidget.dart';
 
 class HomeWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Taekwondo'),
-      ),
-      body: Belt(),
-    );
-  }
-}
 
-class Belt extends StatelessWidget {
-  var words = {
-    'Bam-jumeok': 'Kastanjenæve',
-    'Balnal-deung': 'Inderside af fod',
-    'Sonnal-deung': 'Inderside af knivhånd',
-    'Teok': 'Hage',
-    'Dangyo': 'Trække',
-    'Santeul': 'Bjerg',
-    'Oe-santeul': 'Halvt bjerg',
-    'Pal': '8.',
-    'Geodeureo': 'Støtte',
+  final kup2 = {
+/*
+    'An-chung-joochoom-seogi': 'Indaddrejet hestestand',
+    'Sonnal-deung bakat-makki': 'Udadgående blokering med omvendt knivhånd',
+    'Agwison kaljebi': 'Slag mod hals med agwison',
+    'Han-sonnal area makki': 'Enkelt knivhåndsblokering i lav sektion',
+    'Sonnal area makki': 'Knivhåndsblokering i lav sektion',
+    'Palkoop olryeo-chigi': 'Opadgående albuestød',
+    'Palkoop nareyo-chigi': 'Nedadgående albuestød',
+    'Twieo ieo-chagi': 'Samme spark 2 gange flyvende',
+    'Dwit nakeo-chagi': 'Bagudrettet krogspark',
+*/
+
+    'Jibge-jumeok': 'Pinsetnæve',
+    'Gawison-keut': 'Sakse fingerstik',
+    'Hanson-keut': 'Enkelt fingerstik',
+    'Moeun-dooson-keut': 'Dobbelt fingerstik',
+    'Moeun-seson-keut': 'Tre fingerstik',
+    'Agwison': 'Runding mellem tommel og pegefinger',
+    'Kaljebi': 'Slag mod hals m. agwison',
+    'Ko': 'Niende',
+    'Ship': 'Tiende'
   };
 
-  @override
+  final kup3 = {
+      'Bam-jumeok': 'Kastanjenæve',
+      'Balnal-deung': 'Inderside af fod',
+      'Sonnal-deung': 'Inderside af knivhånd',
+      'Teok': 'Hage',
+      'Dangyo': 'Trække',
+      'Santeul': 'Bjerg',
+      'Oe-santeul': 'Halvt bjerg',
+      'Pal': 'Ottende',
+      'Geodeureo': 'Støtte',
+    };
+
+
+@override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: InkWell(
-        onTap: () async {
-          await showDialog(
-            context: context,
-            builder: (context) => AlertDialog(
-              title:
-                  Text('Der klommer ${words.keys.toList().length} spørgsmål'),
-              content: Text('Vil du starte?'),
-              actions: <Widget>[
-                FlatButton(
-                    onPressed: () => {Navigator.pop(context)},
-                    child: Text('Nej')),
-                FlatButton(
-                    onPressed: () => {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => IngameWidget(
-                                        words: words,
-                                        round: 0,
-                                      )))
-                        },
-                    child: Text('Ja')),
-              ],
-            ),
-          );
-        },
-        child: Container(
-          margin: EdgeInsets.all(10),
-          height: 40,
-          decoration: BoxDecoration(
-              color: Colors.red,
-              border: Border.all(
-                  color: Colors.red[900],
-                  width: 2
-              )
-          ),
-          child: Container(
-            margin: EdgeInsets.symmetric(horizontal: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(
-                  '3.Kup',
-                  style: TextStyle(color: Colors.white),
-                ),
-                Container(
-                  width: 10,
-                  color: Colors.black,
-                )
-              ],
-            ),
-          ),
+      resizeToAvoidBottomInset: true,
+      resizeToAvoidBottomPadding: true,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text('Taekwondo'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Belt(words: kup3, color: Colors.red, snipColor: Colors.black, text: '3.Kup',),
+            Kup2Belt(words: kup2,)
+          ],
         ),
       ),
     );
   }
 }
+
+
---
+++ lib/MakeBelt.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:taekwondopensum/pages/IngameWidget.dart';
+
+class Kup2Belt extends StatelessWidget {
+  final words;
+  Kup2Belt({this.words});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+        onTap: () async {
+          await showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              title:
+              Text('Der kommer ${words.keys.toList().length} spørgsmål'),
+              content: Text('Tryk "OK" for at komme igang'),
+              actions: <Widget>[
+                FlatButton(
+                    onPressed: () => {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => IngameWidget(
+                                words: words,
+                                round: 0,
+                              )))
+                    },
+                    child: Text('OK')),
+              ],
+            ),
+          );
+        },
+        child: Container(
+          margin: EdgeInsets.all(5),
+          height: 50,
+          decoration: BoxDecoration(
+              color: Colors.red,
+              border: Border.all(
+                  color: Colors.red[900],
+                  width: 2
+              )
+          ),
+          child: Container(
+            margin: EdgeInsets.symmetric(horizontal: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  '2.Kup',
+                  style: TextStyle(color: Colors.white),
+                ),
+                Row(
+                  children: <Widget>[
+                    Container(
+                      width: 10,
+                      color: Colors.black,
+                    ),
+                    SizedBox(width: 10,),
+                    Container(
+                      width: 10,
+                      color: Colors.black,
+                    ),
+                    SizedBox(width: 10,),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+    );
+  }
+}
+
+class Belt extends StatelessWidget {
+  final words;
+  String text;
+  final color;
+  Color snipColor;
+  int snip;
+  Belt({this.words, this.color, this.snipColor, this.snip, this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+        onTap: () async {
+          await showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              title:
+              Text('Der kommer ${words.keys.toList().length} spørgsmål'),
+              content: Text('Tryk "OK" for at komme igang'),
+              actions: <Widget>[
+                FlatButton(
+                    onPressed: () => {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => IngameWidget(
+                                words: words,
+                                round: 0,
+                              )))
+                    },
+                    child: Text('OK')),
+              ],
+            ),
+          );
+        },
+        child: Container(
+          margin: EdgeInsets.all(5),
+          height: 50,
+          decoration: BoxDecoration(
+              color: color,
+              border: Border.all(
+                  color: color[900],
+                  width: 2
+              )
+          ),
+            child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      text,
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          width: 10,
+                          color: snipColor,
+                        ),
+                        SizedBox(width: 10,),
+                      ],
+                    ),
+                  ],
+                ),
+            ),
+        ),
+      );}
+}
+
