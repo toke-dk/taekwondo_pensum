aeb2cd0ced63976cead5e138b2e55fd2b8a120f9
Toke Friis <tokefriis@gmail.com>
2021-06-10 07:12:35 +0200

    padding of autocomplete widget works

--- lib/pages/IngameWidget.dart
+++ lib/pages/IngameWidget.dart
@@ -77,6 +77,7 @@ class _IngameWidgetState extends State<IngameWidget> {
     }
 
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         centerTitle: true,
         leading: ExitDialog(),
@@ -100,7 +101,6 @@ class _IngameWidgetState extends State<IngameWidget> {
         )
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
           Column(
             children: <Widget>[
@@ -112,10 +112,10 @@ class _IngameWidgetState extends State<IngameWidget> {
                   Container(
                     margin: EdgeInsets.only(top: 5),
                     child: CircleAvatar(
-                      radius: 22,
+                      radius: 14,
                       child: Text(
                         '${widget.round + 1}',
-                        style: TextStyle(fontSize: 27),
+                        style: TextStyle(fontSize: 16),
                       ),
                     ),
                   ),
@@ -123,14 +123,13 @@ class _IngameWidgetState extends State<IngameWidget> {
                 ],
               ),
               Divider(
-                thickness: 2,
-                height: 20,
+                thickness: 1,
                 indent: 20,
                 endIndent: 20,),
               Container(
                 child: Text(
                   '${_wordsKeys[widget.round]}',
-                  style: TextStyle(fontSize: 25, fontFamily: 'Quicksand', fontWeight: FontWeight.bold),
+                  style: TextStyle(fontSize: 19, fontFamily: 'Quicksand', fontWeight: FontWeight.bold),
                 ),
               ),
              ],
@@ -138,86 +137,135 @@ class _IngameWidgetState extends State<IngameWidget> {
           Container(
             margin: EdgeInsets.symmetric(vertical: 5, horizontal: 8),
             alignment: Alignment.centerRight,
-            child: Column(
-              children: [
-                Autocomplete<String>(
-                  optionsBuilder: (TextEditingValue textEditingValue) {
-                    if (textEditingValue.text == '') {
-                      return const Iterable<String>.empty();
-                    }
-                    return _wordsValues.where((String option) {
-                      return option.toLowerCase().contains(textEditingValue.text.toLowerCase());
-                    });
-                  },
-                  onSelected: (String selection) {
-                    print('You just selected $selection');
-                  },
-                ),
-                SizedBox(height: 50,),
-                /*TextFormField(
-                  textAlign: TextAlign.center,
-                  controller: nameHolder,
-                  onChanged: (value) {
-                    guess = value;
-                  },
-                  autofocus: true,
-                  decoration: InputDecoration(
-                      suffixIcon: IconButton(
-                        onPressed: () => {
-                          setState((){
-                            if (! _showAnswer) {
-                              if (guess.toLowerCase() ==
-                                  widget.words[_wordsKeys[widget.round]].toLowerCase()) {
-                                isAnswer = true;
-                                points += 1;
-                                cloneWords.remove(_wordsKeys[widget.round]);
-                              } else {
-                                isAnswer = false;
-                              }
-                              setState(() {
-                                _showAnswer = !_showAnswer;
-                              });
+            child: Container(
+              margin: EdgeInsets.only(bottom: 300),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Flexible(
+                        child: Autocomplete<String>(
+                          optionsBuilder: (TextEditingValue textEditingValue) {
+                            if (textEditingValue.text == '') {
+                              return const Iterable<String>.empty();
                             }
-                            else {
-                              if (_wordsKeys.length - widget.round - 1 == 0) {
-                                Navigator.pushReplacement(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => ResultWidget(
-                                          remainWords: cloneWords,
-                                          totalWords: widget.words,
-                                        )));
-                              } else {
-                                clearInput();
-                                isAnswer = false;
-                                guess = '';
-                                widget.round += 1;
-                                _showAnswer = !_showAnswer;
-                              }
-                            }
-                          })
+                            return _wordsValues.where((String option) {
+                              return option.toLowerCase().contains(textEditingValue.text.toLowerCase());
+                            });
+                          },
+                          onSelected: (String selection) {
+                            setState(() {
+                              guess = selection;
+                            });
+                          },
+                        ),
+                      ),
+                      SizedBox(height: 50,),
+                      IconButton(
+                            onPressed: () => {
+                              setState((){
+                                if (! _showAnswer) {
+                                  if (guess.toLowerCase() ==
+                                      widget.words[_wordsKeys[widget.round]].toLowerCase()) {
+                                    isAnswer = true;
+                                    points += 1;
+                                    cloneWords.remove(_wordsKeys[widget.round]);
+                                  } else {
+                                    isAnswer = false;
+                                  }
+                                  setState(() {
+                                    _showAnswer = !_showAnswer;
+                                  });
+                                }
+                                else {
+                                  if (_wordsKeys.length - widget.round - 1 == 0) {
+                                    Navigator.pushReplacement(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => ResultWidget(
+                                              remainWords: cloneWords,
+                                              totalWords: widget.words,
+                                            )));
+                                  } else {
+                                    clearInput();
+                                    isAnswer = false;
+                                    guess = '';
+
+                                    widget.round += 1;
+                                    _showAnswer = !_showAnswer;
+                                  }
+                                }
+                              })
+                            },
+                            icon: Icon(Icons.send),
+                          ),
+                      /*TextFormField(
+                        textAlign: TextAlign.center,
+                        controller: nameHolder,
+                        onChanged: (value) {
+                          guess = value;
                         },
-                        icon: Icon(Icons.send),
-                      ),
-                      hintText: 'Skriv dit bud her',
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15),
-                        borderSide: BorderSide(color: Colors.white, width: 2),
-                        //borderRadius: BorderRadius.circular(12)
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: BorderSide(color: isAnswer ? Colors.green : Colors.blue, width: 2),
-                        //borderRadius: BorderRadius.circular(12)
-                      ),
-                      fillColor: Colors.grey[300],
-                      filled: true,
-                      border: InputBorder.none),
-                  style: TextStyle(
-                    fontSize: 15,
+                        autofocus: true,
+                        decoration: InputDecoration(
+                            suffixIcon: IconButton(
+                              onPressed: () => {
+                                setState((){
+                                  if (! _showAnswer) {
+                                    if (guess.toLowerCase() ==
+                                        widget.words[_wordsKeys[widget.round]].toLowerCase()) {
+                                      isAnswer = true;
+                                      points += 1;
+                                      cloneWords.remove(_wordsKeys[widget.round]);
+                                    } else {
+                                      isAnswer = false;
+                                    }
+                                    setState(() {
+                                      _showAnswer = !_showAnswer;
+                                    });
+                                  }
+                                  else {
+                                    if (_wordsKeys.length - widget.round - 1 == 0) {
+                                      Navigator.pushReplacement(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) => ResultWidget(
+                                                remainWords: cloneWords,
+                                                totalWords: widget.words,
+                                              )));
+                                    } else {
+                                      clearInput();
+                                      isAnswer = false;
+                                      guess = '';
+                                      widget.round += 1;
+                                      _showAnswer = !_showAnswer;
+                                    }
+                                  }
+                                })
+                              },
+                              icon: Icon(Icons.send),
+                            ),
+                            hintText: 'Skriv dit bud her',
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                              borderSide: BorderSide(color: Colors.white, width: 2),
+                              //borderRadius: BorderRadius.circular(12)
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              borderSide: BorderSide(color: isAnswer ? Colors.green : Colors.blue, width: 2),
+                              //borderRadius: BorderRadius.circular(12)
+                            ),
+                            fillColor: Colors.grey[300],
+                            filled: true,
+                            border: InputBorder.none),
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),*/
+                    ],
                   ),
-                ),*/
-              ],
+                ],
+              ),
             ),
           ),
         ],
